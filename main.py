1742ca1ab62f1981c64abc8f19948478abe5a4d4
M1tchellC <142175949+M1tchellC@users.noreply.github.com>
2026-02-11 10:56:18 -0500

    import chess

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import requests
 import io
+import chess
 
 app = FastAPI()
 
@@ -224,6 +225,7 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 
 
 
