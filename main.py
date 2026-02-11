7ed10cfb291449029793e0d09a0f153829728cb6
M1tchellC <142175949+M1tchellC@users.noreply.github.com>
2026-02-11 09:42:17 -0500

    imported request

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import requests
 
 app = FastAPI()
 
@@ -222,6 +223,7 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 
 
 
